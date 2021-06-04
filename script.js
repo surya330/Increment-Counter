126885d6f429dad34df528969647c76250e28448
Surya Teja Chatrathi <77150730+surya330@users.noreply.github.com>
2021-06-04 17:28:31 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,29 @@
+const counters = document.querySelectorAll(".text");
+
+counters.forEach(counter => {
+    counter.innerText = 0
+
+    const updateCounter = () => {
+        const target = +counter.getAttribute('data-target');
+        
+        const c = +counter.innerText;
+
+        const increment = target / 200
+
+        if(c < target) {
+            counter.innerText = `${Math.ceil(c + increment)}`
+            setTimeout(updateCounter, 1)
+        } else {
+            counter.innerText = target
+        }
+    }
+
+
+
+
+
+
+
+
+    updateCounter()
+});
